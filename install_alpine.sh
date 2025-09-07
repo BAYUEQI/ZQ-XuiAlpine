0604f9400848035baa87c8f3972ba44c6cf6ba46
八月琪 <zouqi050813@gmail.com>
2025-09-07 12:56:11 +0800

    Update install_alpine.sh

--- install_alpine.sh
+++ install_alpine.sh
@@ -30,7 +30,8 @@ config_after_install() {
     local existing_password=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'password: .+' | awk '{print $2}')
     local existing_webBasePath=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'webBasePath: .+' | awk '{print $2}')
     local existing_port=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'port: .+' | awk '{print $2}')
-    local server_ip=$(curl -s https://api.ipify.org)
+    # 使用api64.ipify.org以同时支持IPv4和IPv6
+    local server_ip=$(curl -s https://api64.ipify.org)
 
     if [[ ${#existing_webBasePath} -lt 4 ]]; then
         if [[ "$existing_username" == "admin" && "$existing_password" == "admin" ]]; then
